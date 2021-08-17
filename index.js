7cd68b293e23c57ca8aa02a1e8892f10970a77ba
Zenki <greyzenchance@gmail.com>
2021-08-17 16:22:40 +0800

    Mobile menu animation close and open

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const mobileMenu = document.querySelector('.toggle-menu');
 const navBar = document.querySelector('nav');
 
 mobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('open');
     navBar.classList.toggle('active');
 })
