fca8425fac47fe7c054a89dba03878c1f61892e6
Zenki <greyzenchance@gmail.com>
2021-08-20 14:16:05 +0800

    Writer version hero page and navigation

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const mobileMenu = document.querySelector('.toggle-menu');
-const navBar = document.querySelector('nav');
+// Variables
 
-mobileMenu.addEventListener('click', () => {
-    mobileMenu.classList.toggle('open');
+const navMenu = document.querySelector('.hamburger-menu');
+const heading = document.querySelector('h1');
+
+// Navigation
+
+navMenu.addEventListener('click', () => {
+    const navBar = document.querySelector('nav');
+    const heroSection = document.querySelector('.hero-section');
+    const greenSmudge = document.querySelector('.green-smudge');
+    const violetSmudge = document.querySelector('.title-background');
+
+    navMenu.classList.toggle('open');
     navBar.classList.toggle('active');
+    heroSection.classList.toggle('shift');
+    greenSmudge.classList.toggle('opacity');
+    violetSmudge.classList.toggle('disappear');
+    heading.classList.toggle('violet');
 })
+
+// Hero Section
+
